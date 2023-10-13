0f3af28b17481876d4d27890da8451ae4b217ec8
Andreas Motl <andreas.motl@panodata.org>
2023-10-14 01:06:03 +0200

    Add implementation from `packaging.version`

---
+++ verlib2/__init__.py
@@ -0,0 +1,18 @@
+# This file is dual licensed under the terms of the Apache License, Version
+# 2.0, and the BSD License. See the LICENSE file in the root of this repository
+# for complete details.
+
+__title__ = "verlib2"
+__summary__ = "A standalone variant of packaging.version, without anything else"
+__uri__ = "https://github.com/panodata/verlib2"
+
+__version__ = "0.1.0"
+
+__author__ = "Donald Stufft and individual contributors"
+__email__ = "donald@stufft.io"
+
+__license__ = "BSD-2-Clause or Apache-2.0"
+__copyright__ = "2014 %s" % __author__
+
+
+from .version import Version  # noqa: F401
