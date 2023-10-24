92a8e209b9041e8427a2b98f035bee686ad3a974
Andreas Motl <andreas.motl@panodata.org>
2023-10-24 15:29:29 +0200

    Add `version` property for backward-compatibility with distutils.version

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -8,3 +8,15 @@ def test_version():
         < Version("1!1.0b1.dev456")
         < Version("1!1.2.rev33+123456")
     )
+
+
+def test_distutils_backward_compatibility():
+    """
+    The `LooseVersion` and `StrictVersion` classes of `distutils.version`
+    had a `version` property, mirroring the `release` property of
+    `packaging.version`.
+
+    This test verifies it is in place.
+    """
+    version = Version("1.0.dev456")
+    assert version.version == (1, 0)
