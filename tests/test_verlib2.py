7ba3b8ca9ec1e2e1775ab909d7d68a522856094e
Andreas Motl <andreas.motl@panodata.org>
2025-02-11 20:49:43 +0100

    distutils: Skip strict-parsing versions like `0.2.1.post3+g2cb3e`

--- tests/test_verlib2.py
+++ tests/test_verlib2.py
@@ -1,5 +1,7 @@
 import sys
 
+import pytest
+
 from verlib2 import __version__
 from verlib2.distutils.version import LooseVersion, StrictVersion
 from verlib2.packaging.version import Version
@@ -10,4 +12,9 @@ def test_version():
         assert Version("0.0.0") <= Version(__version__)
     if sys.version_info >= (3, 8):
         assert LooseVersion("0.0.0") <= __version__
-        assert StrictVersion("0.0.0") <= __version__
+        if "+" in __version__:
+            with pytest.raises(ValueError) as ex:
+                _ = StrictVersion("0.0.0") <= __version__
+            assert ex.match("invalid version number")
+        else:
+            assert StrictVersion("0.0.0") <= __version__
